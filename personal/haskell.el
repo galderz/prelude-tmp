7329dce31712cbe8604a4b5747e49fe2fd7cbaae
Galder Zamarreño <galder@zamarreno.com>
2017-01-23 14:53:22 +0100

    Build haskell using stack

---
+++ personal/haskell.el
@@ -0,0 +1,11 @@
+;;; haskell.el --- Emacs Prelude: Haskell tweaks
+;;
+;;; Commentary:
+;; Personal tweaks of the Haskell mode
+
+;;; Code:
+
+;; Compile stack based projects
+(setq haskell-compile-cabal-build-command "stack build")
+
+;;; haskell.el ends here

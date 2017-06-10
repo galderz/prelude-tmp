39ad6e337fb5841a63593d8345a29cdda2d496e4
Galder Zamarreño <galder@zamarreno.com>
2017-06-10 15:27:11 +0200

    Remove magit GH pulls

--- personal/magit.el
+++ personal/magit.el
@@ -5,9 +5,4 @@
 
 ;;; Code:
 
-(add-to-list 'load-path "~/.emacs.d/personal/lib/s")
-(add-to-list 'load-path "~/.emacs.d/personal/lib/magit-gh-pulls")
-(require 'magit-gh-pulls)
-(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
-
 ;;; magit.el ends here

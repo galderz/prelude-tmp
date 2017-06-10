88b78d79faf9dca4afc39dac9e5a30831eb0a23f
Galder Zamarreño <galder@zamarreno.com>
2017-06-10 15:51:22 +0200

    Add company-ghci for auto-complete

--- personal/haskell.el
+++ personal/haskell.el
@@ -5,8 +5,18 @@
 
 ;;; Code:
 
+
 ;; Install Intero
 (package-install 'intero)
 (add-hook 'haskell-mode-hook 'intero-mode)
+
+
+;; Install Company GHCI
+(package-install 'company-ghci)
+(push 'company-ghci company-backends)
+(add-hook 'haskell-mode-hook 'company-mode)
+;;; To get completions in the REPL
+(add-hook 'haskell-interactive-mode-hook 'company-mode)
+
 
 ;;; haskell.el ends here
